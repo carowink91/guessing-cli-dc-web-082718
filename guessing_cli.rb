6631b1ef6a3fa952f84c4bbf0ee9b3e72e06b18e
angelica winkler <caw400@nyu.edu>
2018-08-10 01:47:30 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,8 @@
 # Code your solution here!
 def run_guessing_game
+  puts "Guess a number between 1 and 6."
   user_num = gets.chomp
-  comp_num = rand().to_i
+  comp_num = rand(1..6).to_i
 
 
   if user_num == comp_num
