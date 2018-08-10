c022739818148dc66940ba9d5aa53fe95274c2a0
angelica winkler <caw400@nyu.edu>
2018-08-10 01:42:07 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,5 +3,8 @@ def run_guessing_game
   user_num = gets.chomp
   comp_num = rand().to_i
 
-  if user_num == generated_number
+  if user_num == comp_num
+    puts "You guessed the correct number!"
+  elsif user_num != comp_num
+    puts "The computer guessed #{comp_num}"
 end
