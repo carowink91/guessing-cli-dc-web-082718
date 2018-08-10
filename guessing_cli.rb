34d2129be0ea42c49cbfd9fac71f123062c2a5ce
angelica winkler <caw400@nyu.edu>
2018-08-10 01:44:27 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,6 @@ def run_guessing_game
       puts "You guessed the correct number!"
     elsif user_num != comp_num
       puts "The computer guessed #{comp_num}"
-    elsif user_num == "exit"
     end
   end
 end
