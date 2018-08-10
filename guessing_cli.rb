8f3cdc89c3d81aef5e2acf5b386e3368cfeae933
angelica winkler <caw400@nyu.edu>
2018-08-10 01:46:07 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,11 +3,12 @@ def run_guessing_game
   user_num = gets.chomp
   comp_num = rand().to_i
 
-  until user_num == "exit"
-    if user_num == comp_num
-      puts "You guessed the correct number!"
-    elsif user_num != comp_num
-      puts "The computer guessed #{comp_num}"
-    end
+
+  if user_num == comp_num
+    puts "You guessed the correct number!"
+  elsif user_num != comp_num
+    puts "The computer guessed #{comp_num}"
+  elsif user_num == "exit"
+    puts "Goodbye"
   end
 end
