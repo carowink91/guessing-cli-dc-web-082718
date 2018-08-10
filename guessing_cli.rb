1e7f6b5aced66bd656181761a34d0e718dfecfb5
angelica winkler <caw400@nyu.edu>
2018-08-10 02:16:46 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,7 @@ user_num = ""
     comp_num = rand(1..6).to_i
     break if user_num == "exit"
       if user_num != comp_num
-        print "The computer guessed #{comp_num}"
+        print "The computer guessed #{comp_num}."
       elsif user_num == comp_num
         print "You guessed the correct number!"
     end
