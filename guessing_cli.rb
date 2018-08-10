d819ea66c0f4a4cab64521bbca31d6a677f67bfa
angelica winkler <caw400@nyu.edu>
2018-08-10 02:36:00 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,11 +1,10 @@
 
 def run_guessing_game
 user_num = ""
-  while user_num
+  while user_num != "exit"
     puts "Guess a number between 1 and 6."
     user_num = gets.chomp
     comp_num = rand(1..6).to_i
-    break if user_num == "exit"
     if user_num == comp_num
       print "You guessed the correct number!"
     elsif user_num != comp_num
