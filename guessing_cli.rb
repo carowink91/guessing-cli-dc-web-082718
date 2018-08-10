e5de16cb6951977bd25b1b192c40261794aa6397
angelica winkler <caw400@nyu.edu>
2018-08-10 02:12:58 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
 
   loop do
     puts "Guess a number between 1 and 6."
-    user_num = gets.downcase.chomp
+    user_num = gets.chomp
     comp_num = rand(1..6).to_i
     break if user_num == "exit"
       if user_num != comp_num
